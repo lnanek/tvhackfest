e887360828b629e422d9f2519eebe209cf4ef958
lnanek <lnanek@gmail.com>
2013-02-07 13:27:24 -0800

    Heat map.

--- assets/www/js/main.js
+++ assets/www/js/main.js
@@ -15,10 +15,17 @@ function onBackButtonDown(e) {
 }
 
 window.echo = function(str, callback) {
+	if ( !cordova || !cordova.exec ) {
+		return;
+	}
+	
     cordova.exec(callback, function(err) {
         callback('Nothing to echo.');
     }, "Echo", "echo", [str]);
 };
+
+var counter = 0;
+var likeTimes = [];
 
 function onDeviceReady() {
     console.log("onDeviceReady()");
@@ -26,7 +33,6 @@ function onDeviceReady() {
     document.addEventListener("backbutton", onBackButtonDown, true);
 
     //var lastTime = new Date();
-    var counter = 0;
     var updateTimer = function() {
     	counter++;
     	$('#showTimer').text("Time: " + counter);
@@ -40,3 +46,31 @@ function onDeviceReady() {
     
 }
 
+function onLikeThis() {
+	likeTimes.push(counter);
+}
+
+var context = document.getElementById('heatmap').getContext('2d');
+
+requestAnimationFrame(update);
+
+function update() {
+	requestAnimationFrame(update);
+	
+	
+		
+	context.fillStyle   = '#000';
+	context.strokeStyle = '#000';
+	context.lineWidth   = 4;
+    context.fillRect(0, 0, context.canvas.width, context.canvas.height);
+    
+    for(var i = 0; i < likeTimes.length; i++) {
+    	var likeTime = likeTimes[i];
+		context.fillStyle   = '#f00';
+		context.strokeStyle = '#f00';
+		context.lineWidth   = 4;
+		var time = (likeTime / counter) * context.canvas.width;
+    	context.fillRect(time, 0, 10, context.canvas.height);
+	}
+    
+}
