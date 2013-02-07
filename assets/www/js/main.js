12a9193653acc71bfc06eb06213206b6ee151e00
ronn <ronn.abueg@gmail.com>
2013-02-07 14:12:56 -0800

    canned comments

--- assets/www/js/main.js
+++ assets/www/js/main.js
@@ -22,13 +22,15 @@ window.echo = function(str, callback) {
   }
 };
 
+var counter = 0;
+var likeTimes = [];
+
 function onDeviceReady() {
   console.log("onDeviceReady()");
     
   document.addEventListener("backbutton", onBackButtonDown, true);
 
   //var lastTime = new Date();
-  var counter = 0;
   var timer = $('.showTimer');
   var updateTimer = function() {
     counter++;
@@ -40,8 +42,40 @@ function onDeviceReady() {
   window.echo("echome", function(echoValue) {
     alert(echoValue == "echome"); // should alert true.
   });
+    
+  window.echo("echome", function(echoValue) {
+    alert(echoValue == "echome"); // should alert true.
+  });
 }
 
+function onLikeThis() {
+  likeTimes.push(counter);
+}
+
+var context = document.getElementById('heatmap').getContext('2d');
+
+requestAnimationFrame(update);
+
+function update() {
+  requestAnimationFrame(update);
+	
+	
+		
+  context.fillStyle   = '#000';
+  context.strokeStyle = '#000';
+  context.lineWidth   = 4;
+  context.fillRect(0, 0, context.canvas.width, context.canvas.height);
+    
+  for(var i = 0; i < likeTimes.length; i++) {
+    var likeTime = likeTimes[i];
+    context.fillStyle   = '#f00';
+    context.strokeStyle = '#f00';
+    context.lineWidth   = 4;
+    var time = (likeTime / counter) * context.canvas.width;
+    context.fillRect(time, 0, 10, context.canvas.height);
+  }
+    
+}
 
 $(document).ready(function() {
   onDeviceReady();
